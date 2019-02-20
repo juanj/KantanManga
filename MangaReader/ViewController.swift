110eee5d32c01c228596d9a2cf2a490aa059a9ba
Juan Jose <1958175+juanj@users.noreply.github.com>
2019-02-20 08:42:38 -0500

    Initial Commit

---
+++ MangaReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MangaReader
+//
+//  Created by admin on 2/20/19.
+//  Copyright © 2019 Bakura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
