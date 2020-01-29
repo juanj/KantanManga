69825852720d2a248203b19d302f6f86f6769060
Juan Jose <1958175+juanj@users.noreply.github.com>
2020-01-29 17:44:23 -0500

    Add convenience init to Manga

--- MangaReader/Model/Manga.swift
+++ MangaReader/Model/Manga.swift
@@ -12,11 +12,22 @@ import CoreData
 
 @objc(Manga)
 public class Manga: NSManagedObject {
-    var coverImage: UIImage?
+    private(set) public var coverImage = UIImage()
+
+    convenience init(context: NSManagedObjectContext, coverData: Data, totalPages: Int16, filePath: String, currentPage: Int16 = 0, createdAt: Date = Date(), lastViewedAt: Date? = nil) {
+        self.init(context: context)
+        self.coverData = coverData
+        self.totalPages = totalPages
+        self.filePath = filePath
+        self.currentPage = currentPage
+        self.createdAt = createdAt
+        self.lastViewedAt = lastViewedAt
+    }
 
     override public func awakeFromFetch() {
         super.awakeFromFetch()
 
+        // Preload image data
         if let data = coverData, let image = UIImage(data: data) {
             coverImage = image
         }
