1aa59a21ce51e191ca1eaa7315208fa998c6e245
Juan Jose <1958175+juanj@users.noreply.github.com>
2020-09-11 10:04:47 -0500

    Show collections as a stack of mangas

--- MangaReader/Views/Library/LibraryViewController.swift
+++ MangaReader/Views/Library/LibraryViewController.swift
@@ -58,13 +58,9 @@ class LibraryViewController: UIViewController {
         collectionView.dataSource = self
         collectionView.contentInset = UIEdgeInsets(top: 50, left: 50, bottom: 50, right: 50)
 
-        collectionView.register(UINib.init(nibName: "MangaCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "MangaCell")
+        collectionView.register(UINib(nibName: "MangaCollectionCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "MangaCell")
 
-        let deleteMenuItem = UIMenuItem(title: "Delete", action: NSSelectorFromString("deleteCollectionCell"))
-        UIMenuController.shared.menuItems = [deleteMenuItem]
-
-        let layout = UICollectionViewFlowLayout()//LibraryCollectionViewLayout()
-        //layout.delegate = self
+        let layout = UICollectionViewFlowLayout()
         collectionView.collectionViewLayout = layout
     }
 
@@ -98,65 +94,31 @@ class LibraryViewController: UIViewController {
 
 extension LibraryViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return max(collections.count, 1)
+        1
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        guard collections.count > 0 else { return 0 }
-        return collections[section].mangas.count
+        return collections.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MangaCell", for: indexPath) as! MangaCollectionViewCell // swiftlint:disable:this force_cast
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MangaCell", for: indexPath) as! MangaCollectionCollectionViewCell // swiftlint:disable:this force_cast
 
-        let manga = collections[indexPath.section].mangas[indexPath.row]
-        manga.loadCoverImage()
-        if let image = manga.coverImage {
-            cell.coverImageView.image = image
-        }
-        cell.pageLabel.text = "\(manga.currentPage)/\(manga.totalPages)"
+        let collection = collections[indexPath.row]
+        let mangas = collection.mangas.prefix(3)
+        mangas.forEach { $0.loadCoverImage() }
+        let images = mangas.map { $0.coverImage ?? UIImage() }
+        cell.setImages(images)
+        cell.nameLabel.text = collection.name
 
         return cell
     }
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        guard let cell = collectionView.cellForItem(at: indexPath) as? MangaCollectionViewCell else { return }
-        let frame = cell.convert(cell.coverImageView.frame, to: view)
-        let manga = collections[indexPath.section].mangas[indexPath.row]
-        delegate?.didSelectManga(self, manga: manga, cellFrame: frame)
+        // Open collection
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let manga = collections[indexPath.section].mangas[indexPath.row]
-        if let image = manga.coverImage {
-            let height = heightForImageWith(maxWidth: 200, maxHeight: 263, image: image) + 37
-            return CGSize(width: 200, height: height)
-        }
-
-        // Default size
         return CGSize(width: 200, height: 300)
-    }
-
-    func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-
-    func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false//return action == NSSelectorFromString("deleteCollectionCell")
-    }
-
-    func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-        delegate?.didSelectDeleteManga(self, manga: collections[indexPath.section].mangas[indexPath.row])
-    }
-}
-
-extension LibraryViewController: LibraryCollectionViewLayoutDelegate {
-    func collectionView(_ collectionView: UICollectionView, heightForMangaAtIndexPath indexPath: IndexPath) -> CGFloat {
-        let manga = collections[indexPath.section].mangas[indexPath.row]
-        if let image = manga.coverImage {
-            let height = heightForImageWith(maxWidth: 200, maxHeight: 263, image: image) + 37
-            return height
-        }
-        return 300
     }
 }
---
+++ MangaReader/Views/Library/MangaCollectionCollectionViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MangaCollectionCollectionViewCell.swift
+//  Kantan-Manga
+//
+//  Created by Juan on 11/09/20.
+//
+
+import UIKit
+
+class MangaCollectionCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageViews: [UIImageView]!
+    @IBOutlet weak var nameLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        imageViews = imageViews.sorted(by: { $0.tag < $1.tag })
+        for imageView in imageViews {
+            imageView.transform = CGAffineTransform(rotationAngle: .random(in: -0.15...0.15))
+        }
+    }
+
+    func setImages(_ images: [UIImage]) {
+        for (image, imageView) in zip(images, imageViews) {
+            imageView.image = image
+        }
+    }
+
+}
