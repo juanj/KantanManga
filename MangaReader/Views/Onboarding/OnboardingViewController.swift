c781f68e97d35f71e38c7dcd1c87837697a06d14
Juan Jose <1958175+juanj@users.noreply.github.com>
2020-10-11 23:12:16 -0500

    Connect page control

--- MangaReader/Views/Onboarding/OnboardingViewController.swift
+++ MangaReader/Views/Onboarding/OnboardingViewController.swift
@@ -8,4 +8,19 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        scrollView.delegate = self
+    }
+}
+
+extension OnboardingViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let page = min(round(scrollView.contentOffset.x / scrollView.frame.width), 3)
+        pageControl.currentPage = Int(page)
+      }
 }
