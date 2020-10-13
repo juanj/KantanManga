d8d9f36441f3fd043dff9e6cb409d7a919fd2929
Juan Jose <1958175+juanj@users.noreply.github.com>
2020-10-13 09:01:53 -0500

    Add last intro view

--- MangaReader/Views/Onboarding/OnboardingViewController.swift
+++ MangaReader/Views/Onboarding/OnboardingViewController.swift
@@ -7,19 +7,52 @@
 
 import UIKit
 
+protocol OnboardingViewControllerDelegate: AnyObject {
+    func didSelectLoadManga(_ onboardingViewController: OnboardingViewController)
+    func didSelectNotNow(_ onboardingViewController: OnboardingViewController)
+}
+
 class OnboardingViewController: UIViewController {
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var demoView: DemoOcrView!
+    @IBOutlet weak var loadMangaButton: UIButton!
+    @IBOutlet weak var notNowButton: UIButton!
 
-    private var shapeLayer: CAShapeLayer!
+    private weak var delegate: OnboardingViewControllerDelegate?
+    init(delegate: OnboardingViewControllerDelegate) {
+        self.delegate = delegate
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         scrollView.delegate = self
+        configureStyle()
+    }
+
+    private func configureStyle() {
         demoView.layer.shadowRadius = 5
         demoView.layer.shadowColor = UIColor.black.cgColor
         demoView.layer.shadowOpacity = 0.5
+
+        loadMangaButton.layer.cornerRadius = 15
+
+        let attributedString = NSAttributedString(string: notNowButton.titleLabel?.text ?? "", attributes: [.foregroundColor: UIColor.darkGray, .font: UIFont.systemFont(ofSize: 12), .underlineStyle: NSUnderlineStyle.single.rawValue])
+        notNowButton.setAttributedTitle(attributedString, for: .normal)
+    }
+
+    @IBAction func loadDemoManga(_ sender: Any) {
+        delegate?.didSelectLoadManga(self)
+    }
+
+    @IBAction func nowNow(_ sender: Any) {
+        delegate?.didSelectNotNow(self)
     }
 }
 
@@ -31,7 +64,7 @@ extension OnboardingViewController: UIScrollViewDelegate {
 
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         let page = min(round(scrollView.contentOffset.x / scrollView.frame.width), 2)
-        if page == 2 {
+        if page == 1 {
             demoView.startAnimation()
         }
     }
