1f8f2a99f58365ded1bc05128f101652a69f47dd
Juan Jose <1958175+juanj@users.noreply.github.com>
2020-10-17 12:54:19 -0500

    Change FocusOverlayView circle to anchor to view

--- MangaReader/Views/Custom Views/FocusOverlayView.swift
+++ MangaReader/Views/Custom Views/FocusOverlayView.swift
@@ -13,17 +13,17 @@ protocol FocusOverlayViewDelegate: AnyObject {
 }
 
 class FocusOverlayView: UIView {
-    var circleCenter: CGPoint = .zero {
+    var centerOn: UIView {
         didSet {
             refreshOverlay()
         }
     }
-    var circleRadius: CGFloat = 100 {
+    var circleRadius: CGFloat = 50 {
         didSet {
             refreshOverlay()
         }
     }
-    var overlayColor: CGColor = UIColor.gray.withAlphaComponent(0.5).cgColor {
+    var overlayColor: CGColor = UIColor.black.withAlphaComponent(0.7).cgColor {
         didSet {
             overlayLayer.fillColor = overlayColor
         }
@@ -32,9 +32,9 @@ class FocusOverlayView: UIView {
     weak var delegate: FocusOverlayViewDelegate?
     private let overlayLayer = CAShapeLayer()
 
-    init(delegate: FocusOverlayViewDelegate, circleCenter: CGPoint, circleRadius: CGFloat) {
+    init(delegate: FocusOverlayViewDelegate, centerOn view: UIView, circleRadius: CGFloat) {
         self.delegate = delegate
-        self.circleCenter = circleCenter
+        centerOn = view
         self.circleRadius = circleRadius
         super.init(frame: .zero)
 
@@ -54,6 +54,9 @@ class FocusOverlayView: UIView {
     }
 
     private func pathForOverlay() -> CGPath {
+        guard let circleCenter = centerOn.superview?.convert(centerOn.center, to: self) else {
+            return CGPath(rect: .zero, transform: nil)
+        }
         let path = UIBezierPath(rect: frame)
         let circle = UIBezierPath(ovalIn: CGRect(x: circleCenter.x - circleRadius, y: circleCenter.y - circleRadius, width: circleRadius * 2, height: circleRadius * 2))
         path.append(circle.reversing())
@@ -66,12 +69,12 @@ class FocusOverlayView: UIView {
     }
 
     @objc private func trigger(tapGesture: UITapGestureRecognizer) {
+        guard let circleCenter = centerOn.superview?.convert(centerOn.center, to: self) else { return }
         let touch = tapGesture.location(in: self)
         let term1 = pow(touch.x - circleCenter.x, 2) / pow(circleRadius, 2)
         let term2 = pow(touch.y - circleCenter.y, 2) / pow(circleRadius, 2)
         if term1 + term2 <= 1 {
             delegate?.didInteractWithContent(self)
-            // TODO: Pass event down
         } else {
             delegate?.didTouchOutside(self)
         }
