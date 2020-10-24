8f2b2dc7c4f1993e555a7ab8544b6191533ad590
Juan Jose <1958175+juanj@users.noreply.github.com>
2020-10-24 15:51:46 -0500

    Add kanji-radical conversion table

--- Kantan-Keyboard/Model/Radical.swift
+++ Kantan-Keyboard/Model/Radical.swift
@@ -11,4 +11,34 @@ struct Radical: Equatable {
     let character: String
     let strokeCount: Int
     let rowId: Int64
+
+    func onlyRadicalPart() -> String {
+        // Radkfile use a kanji to represent these radicals
+        let exceptions = [
+            "化": "⺅",
+            "个": "𠆢",
+            "并": "丷",
+            "刈": "⺉",
+            "乞": "𠂉",
+            "込": "⻌",
+            "尚": "⺌",
+            "忙": "⺖",
+            "扎": "⺘",
+            "汁": "⺡",
+            "犯": "⺨",
+            "艾": "⺾",
+            "邦": "⻏",
+            "阡": "⻖",
+            "老": "⺹",
+            "杰": "⺣",
+            "礼": "⺭",
+            "疔": "疒",
+            "禹": "禸",
+            "初": "⻂",
+            "買": "⺲",
+            "滴": "啇"
+        ]
+
+        return exceptions[character] ?? character
+    }
 }
