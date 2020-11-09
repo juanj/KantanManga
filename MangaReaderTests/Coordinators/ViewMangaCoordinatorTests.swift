87e7c1e7fa03373ef9e02c00126c8fad34e19202
Juan Jose <1958175+juanj@users.noreply.github.com>
2020-11-09 18:02:05 -0500

    Refactor ViewMangaCoordinatorTests

--- MangaReaderTests/Coordinators/ViewMangaCoordinatorTests.swift
+++ MangaReaderTests/Coordinators/ViewMangaCoordinatorTests.swift
@@ -9,69 +9,28 @@ import XCTest
 @testable import Kantan_Manga
 
 class ViewMangaCoordinatorTests: XCTestCase {
-    var navigation: Navigable!
-    var delegate: FakeViewMangaCoordinatorDelegate! // swiftlint:disable:this weak_delegate
-    var viewMangaCoordinator: ViewMangaCoordinator!
-    var ocr: MockOCR!
-    var manga: Manga!
 
-    /*override func setUp() {
-        super.setUp()
-        CoreDataManager.sharedManager.deleteAllData()
-        navigation = MockNavigationController()
-        delegate = MockViewMangaCoordinatorDelegate()
-        ocr = MockOCR()
-        manga = CoreDataManager.sharedManager.insertManga(name: "Test Manga", coverData: Data(), totalPages: 100, filePath: "test.cbz")!
-        viewMangaCoordinator = ViewMangaCoordinator(navigation: navigation, manga: manga, delegate: delegate, originFrame: .zero, ocr: ocr)
-    }
-
-    func testCallingStartPushesViewController() {
+    func testStart_withEmptyNavigation_pushesMangaViewController() {
+        let mockNavigation = FakeNavigation()
+        let viewMangaCoordinator = ViewMangaCoordinator(navigation: mockNavigation, coreDataManager: FakeCoreDataManager(), manga: Manga(), delegate: FakeViewMangaCoordinatorDelegate(), originFrame: .zero, ocr: FakeImageOcr())
         viewMangaCoordinator.start()
-        XCTAssertNotNil(navigation.viewControllersTest.last as? MangaViewController)
+        XCTAssertNotNil(mockNavigation.viewControllers.last as? MangaViewController)
     }
 
     // MARK: MangaViewControllerDelegate
-    func testCallingDidTapTogglesFullscreen() {
+    func testMangaViewControllerDelegateDidTap_togglesFullscreen() {
+        let stubNavigation = FakeNavigation()
+        let viewMangaCoordinator = ViewMangaCoordinator(navigation: stubNavigation, coreDataManager: FakeCoreDataManager(), manga: Manga(), delegate: FakeViewMangaCoordinatorDelegate(), originFrame: .zero, ocr: FakeImageOcr())
         viewMangaCoordinator.start()
-        guard let mangaView = navigation.viewControllersTest.first as? MangaViewController else {
-            XCTAssertFalse(true)
+
+        guard let mangaView = stubNavigation.viewControllers.last as? MangaViewController else {
+            XCTAssertTrue(false)
             return
         }
-        _ = mangaView.view // Load view
-        XCTAssertTrue(mangaView.prefersStatusBarHidden)
+        _ = mangaView // Load view
+
         viewMangaCoordinator.didTapPage(mangaView, pageViewController: PageViewController(delegate: nil, pageSide: .center, pageNumber: 0))
+
         XCTAssertFalse(mangaView.prefersStatusBarHidden)
     }
-
-    func testCallingBackPopsEndEnd() {
-        viewMangaCoordinator.start()
-        guard let mangaView = navigation.viewControllersTest.first as? MangaViewController else {
-            XCTAssertFalse(true)
-            return
-        }
-        XCTAssertTrue(navigation.viewControllersTest.count == 1)
-        viewMangaCoordinator.back(mangaView)
-        XCTAssertTrue(navigation.viewControllersTest.count == 0)
-        XCTAssertTrue(delegate.didEndCalled)
-    }
-
-    func testCallingDidSelectSectionCallsOcr() {
-        viewMangaCoordinator.start()
-        guard let mangaView = navigation.viewControllersTest.first as? MangaViewController else {
-            XCTAssertFalse(true)
-            return
-        }
-        _ = mangaView.view // Load view
-        let image = UIImage()
-        viewMangaCoordinator.didSelectSectionOfImage(mangaView, image: image)
-        XCTAssertEqual(ocr.image, image)
-    }
-
-    // MARK: UINavigationControllerDelegate
-    func testCustomTransitionForPop() {
-        var animation = viewMangaCoordinator.navigationController(navigation, animationControllerFor: .pop, from: UIViewController(), to: UIViewController())
-        XCTAssertNil(animation)
-        animation = viewMangaCoordinator.navigationController(navigation, animationControllerFor: .pop, from: CollectionViewController(delegate: MockCollectionViewControllerDelgate(), collection: MangaCollection(), sourcePoint: .zero, initialRotations: []), to: UIViewController())
-        XCTAssertTrue(animation is OpenMangaAnimationController)
-    }*/
 }
