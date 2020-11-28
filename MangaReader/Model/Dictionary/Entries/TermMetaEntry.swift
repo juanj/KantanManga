ca6f5bbf0d6eadcf8d9d6fcae2fe7df7126c080c
Juan Jose <1958175+juanj@users.noreply.github.com>
2020-11-28 14:32:42 -0500

    Add decoding code

--- MangaReader/Model/Dictionary/Entries/TermMetaEntry.swift
+++ MangaReader/Model/Dictionary/Entries/TermMetaEntry.swift
@@ -13,7 +13,7 @@ struct TermMetaEntry {
         case pitch(reading: String, pitches: [PitchAccent])
     }
 
-    struct PitchAccent {
+    struct PitchAccent: Decodable {
         let position: Int
         let tags: [String]?
     }
@@ -25,5 +25,58 @@ struct TermMetaEntry {
 extension TermMetaEntry: CustomStringConvertible {
     var description: String {
         return "\(character), \(mode)"
+    }
+}
+
+extension TermMetaEntry: Decodable {
+    private enum FreqKeys: String, CodingKey {
+        case reading, frequency
+    }
+
+    private enum PitchKeys: String, CodingKey {
+        case reading, pitches
+    }
+
+    init(from decoder: Decoder) throws {
+        var values = try decoder.unkeyedContainer()
+        character = try values.decode(String.self)
+
+        let type = try values.decode(String.self)
+        switch type {
+        case "freq":
+            do {
+                // If Int or String
+                var frequency: Int
+                if let freq = try? values.decode(Int.self) {
+                    frequency = freq
+                } else {
+                    let frequencyString = try values.decode(String.self)
+                    frequency = Int(frequencyString) ?? 0
+                }
+                mode = .freq(frequency: frequency, reading: nil)
+            } catch {
+                // If Object
+                let item = try values.nestedContainer(keyedBy: FreqKeys.self)
+
+                var frequency: Int
+                if let freq = try? item.decode(Int.self, forKey: .frequency) {
+                    frequency = freq
+                } else {
+                    let frequencyString = try item.decode(String.self, forKey: .frequency)
+                    frequency = Int(frequencyString) ?? 0
+                }
+
+                let reading = try item.decode(String.self, forKey: .reading)
+                mode = .freq(frequency: frequency, reading: reading)
+            }
+        case "pitch":
+            let item = try values.nestedContainer(keyedBy: PitchKeys.self)
+            let reading = try item.decode(String.self, forKey: .reading)
+            let pitches = try item.decode([PitchAccent].self, forKey: .pitches)
+
+            mode = .pitch(reading: reading, pitches: pitches)
+        default:
+            mode = .freq(frequency: 0, reading: nil)
+        }
     }
 }
