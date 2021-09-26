f024734cfe8dd31dac3c8e5beb57be33bd4cab1f
Juan Jose <1958175+juanj@users.noreply.github.com>
2021-09-25 19:18:00 -0500

    Create AnkiSyncCardOperation

---
+++ MangaReader/Anki/AnkiConnect/AnkiSyncCardOperation.swift
@@ -0,0 +1,84 @@
+//
+//  AnkiSyncCardOperation.swift
+//  Kantan-Manga
+//
+//  Created by Juan on 25/09/21.
+//
+
+import Foundation
+
+class AnkiSyncCardOperation: Operation {
+    private enum State {
+        case initial, started, done
+    }
+
+    override var isAsynchronous: Bool { true }
+    override var isExecuting: Bool { state == .started }
+    override var isFinished: Bool { state == .done }
+
+    private var state: State = .initial {
+        willSet {
+            willChangeValue(forKey: "isExecuting")
+            willChangeValue(forKey: "isFinished")
+        }
+        didSet {
+            didChangeValue(forKey: "isExecuting")
+            didChangeValue(forKey: "isFinished")
+        }
+    }
+    private(set) var error: Error?
+    private(set) var noteId: UInt?
+
+    let sentence: Sentence
+    private let config: AnkiConfig
+    private let manager: AnkiConnectManager
+    init(sentence: Sentence, config: AnkiConfig, manager: AnkiConnectManager) {
+        self.sentence = sentence
+        self.config = config
+        self.manager = manager
+    }
+
+    override func start() {
+        guard !isCancelled else {
+            state = .done
+            return
+        }
+        state = .started
+        main()
+    }
+
+    override func main() {
+        let fields = [
+            config.sentenceField: sentence.sentence,
+            config.definitionField: addAnkiNewLines(to: sentence.definition)
+        ]
+
+        var picture: CreateNoteRequest.Picture?
+        if let imageData = sentence.imageData, let imageField = config.imageField {
+            picture = CreateNoteRequest.Picture(
+                filename: "\(sentence.sentence).png",
+                data: imageData.base64EncodedString(),
+                fields: [imageField]
+            )
+        }
+
+        manager.addNoteWith(
+            model: config.note,
+            deck: config.deck,
+            fields: fields,
+            picture: picture
+        ) { [weak self] result in
+            switch result {
+            case let .success(noteId):
+                self?.noteId = noteId
+            case let .failure(error):
+                self?.error = error
+            }
+            self?.state = .done
+        }
+    }
+
+    private func addAnkiNewLines(to string: String) -> String {
+        return string.replacingOccurrences(of: "\n", with: "</br>")
+    }
+}
